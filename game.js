952ddb5955a64b92b9406ea6d20ec430014004ff
Sabbir Hasan <sabbirh1000@gmail.com>
2024-05-04 10:21:27 +0600

    Implement logic for rock-paper-scissor game
    It's a console version of the game. take human input from promt.

---
+++ game.js
@@ -0,0 +1,98 @@
+"use strict";
+
+function getComputerChoice() {
+
+    const choice = Math.floor((Math.random() * 3));
+
+    if (choice == 0) {
+        return "rock";
+    }
+    else if (choice == 1) {
+        return "paper";
+    }
+
+    return "scissors"
+}
+
+function getHumanChoice() {
+    let choice = prompt("Hey Enter your choice : ");
+    return choice.toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+
+        console.log("round is tie!");
+        return;
+    }
+
+    if (humanChoice == "rock") {
+
+        if (computerChoice == "scissors") {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            return true;
+        }
+        else {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            return false;
+        }
+    }
+    else if (humanChoice == "paper") {
+
+        if (computerChoice == "rock") {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            return true;
+        }
+        else {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            return false;
+        }
+    }
+    else {
+
+        if (computerChoice == "paper") {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            return true;
+        }
+        else {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            return false;
+        }
+    }
+}
+
+function playGame() {
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i = 1; i <= 5; i++) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+
+        const isHumanWinner = playRound(humanChoice, computerChoice);
+
+        if(isHumanWinner == undefined) {
+            continue;
+        }
+
+        if(isHumanWinner) {
+            humanScore++;
+        }
+        else {
+            computerScore++;
+        }
+    }
+
+    if(humanScore == computerScore) {
+        console.log("Match tied!");
+    }
+    else if(humanScore > computerScore) {
+        console.log("You winner!")
+    }
+    else {
+        console.log("Computer Winner!");
+    }
+}
+
+playGame();
